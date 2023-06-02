bfd6d968c91cc9fddc225d10288279894bcb7c10
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-06-02 21:25:14 +0800

    feat: Added Point for experience and skill points

---
+++ RailwayCo/Assets/Scripts/GameLogic/Point.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Point
+{
+    private int pointValue;
+    public int PointValue { get => pointValue; private set => pointValue = value; }
+
+    public Point(int pointValue)
+    {
+        PointValue = pointValue;
+    }
+
+    public Dictionary<string, string> AddPointValue(int pointValue)
+    {
+        Dictionary<string, string> result = new()
+        {
+            { "old", PointValue.ToString() }
+        };
+
+        PointValue += pointValue;
+        result.Add("new", PointValue.ToString());
+
+        return result;
+    }
+
+    public Dictionary<string, string> RemovePointValue(int pointValue)
+    {
+        Dictionary<string, string> result = new()
+        {
+            { "old", PointValue.ToString() }
+        };
+
+        PointValue -= pointValue;
+        result.Add("new", PointValue.ToString());
+
+        return result;
+    }
+}
