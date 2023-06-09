3d08dacb071e70d3e4fbc2918b8d8bda81a3b913
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2023-06-10 03:15:04 +0800

    [WIP] refactor + feat: Refactor TrainMovement
    Also include some working funtionality to identify curved tracks and perform a rotation of 90 deg

--- RailwayCo/Assets/Scripts/TrainMovement/TrainMovement.cs
+++ RailwayCo/Assets/Scripts/TrainMovement/TrainMovement.cs
@@ -4,116 +4,246 @@ using UnityEngine;
 
 public class TrainMovement : MonoBehaviour
 {
-    public new Rigidbody2D rigidbody;
-    private float maxSpeed = 5; // Change to enum values once we have more trains
-    private float acceleration = 1; // This acceleration Value will vary once we have different trains
-    private float accelerationCopy;
+    public Rigidbody2D trainRigidbody;
+
+    // Values are in Absolute terms (No direction)
+    private float maxSpeed = 10f; // TODO: Read from Train's Attributes
+    private float acceleration = 1; // TODO: Read from Train's attributes
     private float currentSpeed = 0;
-    private string accelerationDirn;
+
     private string currentStation; // String for now, to replace with station ID.
+    // Need a boolean to deal with the slowdown for entering the station. (Need size, speed and stuff...)
+        // Need to differentiate the state of the train, whether it is slowing, or has fully stopped.
+    // TODO in future (With Station): Deploy and move off in the right direction.
 
+    private Direction movementDirn;
+    private CurveType curveType;
 
+    private Transform[] curvedPath;
+    private float degreesRotated = 0;
 
-    private void Start()
+    enum Direction
     {
-        accelerationCopy = acceleration;
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST,
+    }
 
+    enum CurveType
+    {
+        RIGHTUP,
+        RIGHTDOWN,
+        LEFTUP,
+        LEFTDOWN,
+        STRAIGHT
     }
 
     void Update()
     {
-        moveTrain();
+        // TODO: Add override situation when train is entering the station
+        if (currentSpeed > 0)
+        {
+            currentSpeed += acceleration * Time.deltaTime;
+        }
+        if (currentSpeed > maxSpeed)
+        {
+            currentSpeed = maxSpeed;
+        }
     }
 
     public void departTrain()
     {
         // Will assume the train starts moving to the right.
         // To update Logic on which way to depart once stations' relationship are established.
-
+        movementDirn = Direction.EAST;
+        curveType = CurveType.STRAIGHT;
         currentSpeed += acceleration * Time.deltaTime;
-
+        StartCoroutine(moveTrain());
     }
 
-    public void moveTrain()
+    private IEnumerator moveTrain()
     {
-        if (currentSpeed == 0) return;
-
-        accelerationDirn = "right";
-
-        if (accelerationDirn == "right")
+        // TODO: Set override: End this MoveTrain Enumerator when train is entering the station.
+        while (currentSpeed > 0)
         {
-            if (currentSpeed > 0)
+            switch (curveType)
             {
-                currentSpeed += accelerationCopy * Time.deltaTime;
+                // Perform Checks in the respective functions
+                case CurveType.STRAIGHT:
+                    moveTrainStraight(movementDirn);
+                    break;
+                case CurveType.RIGHTUP:
+                    yield return StartCoroutine(moveTrainRightUp(movementDirn));
+                    break;
+                //case CurveType.RIGHTDOWN:
+                //    moveTrainRightDown();
+                //    break;
+                //case CurveType.LEFTUP:
+                //    moveTrainLeftUp();
+                //    break;
+                //case CurveType.LEFTDOWN:
+                //    moveTrainLeftDown();
+                //    break;
+                default:
+                    Debug.LogError("[Train] MoveTrain Switch Case Not Implemented Error");
+                    yield break;
             }
-
-            if (currentSpeed > maxSpeed)
-            {
-                currentSpeed = maxSpeed;
-            }
-
-            if (currentSpeed < 0)
-            {
-
-                currentSpeed = 0;
-                Debug.Log("Train has come to a stop");
-                accelerationCopy = Mathf.Abs(acceleration);
-
-            }
-
-
-            rigidbody.velocity = new Vector2(currentSpeed, 0);
-
+            yield return null;
         }
+        currentSpeed = 0;
+    }
 
 
+    private void moveTrainStraight(Direction currentDirn)
+    {
+        switch (currentDirn)
+        {
+            case Direction.NORTH:
+                trainRigidbody.velocity = new Vector2(0, currentSpeed);
+                break;
+            case Direction.SOUTH:
+                trainRigidbody.velocity = new Vector2(0, -currentSpeed);
+                break;
+            case Direction.EAST:
+                trainRigidbody.velocity = new Vector2(currentSpeed, 0);
+                break;
+            case Direction.WEST:
+                trainRigidbody.velocity = new Vector2(-currentSpeed, 0);
+                break;
+            default:
+                Debug.LogError($"[Train] {this.name}: Invalid Direction being used to move in a straight line");
+                break;
+        }
+    }
 
+    private IEnumerator moveTrainRightUp(Direction currentDirn)
+    {
+ 
+        if (currentDirn == Direction.EAST)
+        {
+            yield return StartCoroutine(moveRotate(true));
+            movementDirn = Direction.NORTH;
+
+        } 
+        else if (currentDirn == Direction.SOUTH) 
+        {
+            yield return StartCoroutine(moveRotate(false));
+            movementDirn = Direction.WEST;
+        }
         else
         {
-            if (currentSpeed < 0)
-            {
-                currentSpeed -= accelerationCopy * Time.deltaTime;
-            }
+            degreesRotated = 0;
+            Debug.LogError("Invalid Direction Setting for the train to rotate!");
+            yield break;
+        }
 
-            if (currentSpeed < -maxSpeed)
-            {
-                currentSpeed = -maxSpeed;
-            }
-            if (currentSpeed > 0)
-            {
-
-                currentSpeed = 0;
-                Debug.Log("Train has come to a stop");
-                accelerationCopy = Mathf.Abs(acceleration);
-
-            }
-            rigidbody.velocity = new Vector2(currentSpeed, 0);
+        // This should be reached only after moveRotate has finished.
+        if (curveType != CurveType.STRAIGHT)
+        {
+            Debug.LogError("moveRotate did not set the curve type from curved to straight");
         }
     }
 
+    //private void moveTrainRightDown()
+    //{
+       
+    //}
+
+    //private void moveTrainLeftUp()
+    //{
+    //}
+
+    //private void moveTrainLeftDown()
+    //{
+    //}
+
+  
+    private IEnumerator moveRotate(bool rotateLeft)
+    {
+        while (degreesRotated < 90) { 
+            //trainRigidbody.velocity = new Vector2(0, currentSpeed);
+            //float trainRotation = trainRigidbody.rotation;
+            float rotationDelta = currentSpeed * 0.015f;
+            trainRigidbody.rotation = degreesRotated + rotationDelta;
+
+            //Debug.Log(curvedPath[i].position);
+
+
+            degreesRotated += rotationDelta;
+            yield return null;
+        }
+
+        // Rotation Finish Condition
+        curvedPath = null;
+        curveType = CurveType.STRAIGHT;
+        degreesRotated = 0;
+        yield break;
+    }
+
+
+    // Sets the relevant flags when entering a track or station.
+    // Also populate the waypoints if the track is curved so that moveRotate function can utilise it
     private void OnTriggerEnter2D(Collider2D other)
     {
+        // TODO: Change functionality, instead of performing logic here, just set flags. Move this to some sort of deceleration function or smth
         if (other.tag == "Station")
         {
             currentStation = other.name;
 
             if (currentSpeed > 0)
-            { 
-                accelerationCopy = acceleration * -6;
+            {
+                currentSpeed -= acceleration * Time.deltaTime;
             }
         }
+
+        // Sets the relevant flags so that the MoveTrain function will know how to divert code execution
+        // TODO: Switch this to a switch statement... 
+        if (other.tag == "Track_Curved_RU")
+        {
+            curveType = CurveType.RIGHTUP;
+
+            // TOOD: Abstract this part out into a function, to DRY it
+            int childCount = other.transform.childCount;
+            curvedPath = new Transform[childCount];
+            for (int i=0; i< childCount; i++)
+            {
+                curvedPath[i] = other.transform.GetChild(i);
+            }
+        }
+
+        if (other.tag == "Track_Curved_RD")
+        {
+            curveType = CurveType.RIGHTDOWN;
+        }
+
+        if (other.tag == "Track_Curved_LU")
+        {
+            curveType = CurveType.LEFTUP;
+        }
+        if (other.tag == "Track_Curved_LD")
+        {
+            curveType = CurveType.LEFTDOWN;
+        }
+
+        if (other.tag == "Track_LR" || other.tag == "Track_TD")
+        {
+            curveType = CurveType.STRAIGHT;
+        }
+
+        // Debug Purposes only
+        if (other.tag.Contains("Track"))
+        {
+            Debug.Log($"[Train] {this.name}: Has entered the track {other.tag}");
+        }
     }
+
 
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.tag == "Station")
         { 
-            Debug.Log($"[Train] {this.name} : Exited station {currentStation}.");
             currentStation = null;
         }
     }
-
-
-
-
 }
