078535e7c96a2466de190b8f7359cfa08bbfee74
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-06-10 01:49:57 +0800

    feat: added various login methods

--- RailwayCo/Assets/Scripts/Playfab/AuthManager.cs
+++ RailwayCo/Assets/Scripts/Playfab/AuthManager.cs
@@ -1,39 +1,115 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
 
-public class LoginManager : MonoBehaviour
+public class AuthManager
 {
-    public Text emailText;
-    public Text passwordText;
-    public InputField emailInput;
-    public InputField passwordInput;
+    private string sessionTicket;
+    private readonly GetPlayerCombinedInfoRequestParams playerReqParams = new()
+    {
+        GetPlayerProfile = true,
+        GetPlayerStatistics = true,
+        GetTitleData = true,
+        GetUserAccountInfo = true,
+        GetUserData = true,
+        GetUserInventory = true,
+        GetUserReadOnlyData = true,
+        GetUserVirtualCurrency = true,
+        PlayerStatisticNames = null,
+        ProfileConstraints = null,
+        TitleDataKeys = null,
+        UserDataKeys = null,
+        UserReadOnlyDataKeys = null
+    };
+
+    public string SessionTicket { get => sessionTicket; set => sessionTicket = value; }
+
+    public AuthManager()
+    {
+        // TODO: Future additional platform support
+            // Fetch exisiting SessionTicket from local database
+            // PlayFabClientAPI.IsClientLoggedIn()
+            // Sessions only persist for 24 hours
+        SessionTicket = "";
+    }
+
+    public bool IsLoggedIn() => PlayFabClientAPI.IsClientLoggedIn();
 
     public void LoginWithCustomID()
     {
+        LoginType loginType = LoginType.CustomID;
         var request = new LoginWithCustomIDRequest
         {
             CustomId = SystemInfo.deviceUniqueIdentifier,
-            CreateAccount = true
+            CreateAccount = true,
+            InfoRequestParameters = playerReqParams
         };
-        PlayFabClientAPI.LoginWithCustomID(request, OnCustomIDLoginSuccess, OnCustomIDLoginError);
+        PlayFabClientAPI.LoginWithCustomID(
+            request,
+            (loginResult) => OnLoginSuccess(loginResult, loginType),
+            (playFabError) => OnLoginError(playFabError, loginType));
     }
 
-
-
-
-    void OnCustomIDLoginSuccess(LoginResult loginResult)
+    public void LoginWithEmailAddress(string email, string password)
     {
-        Debug.Log("Custom ID Account created successfully");
+        LoginType loginType = LoginType.EmailAddress;
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = email,
+            Password = password,
+            InfoRequestParameters = playerReqParams
+        };
+        PlayFabClientAPI.LoginWithEmailAddress(
+            request,
+            (loginResult) => OnLoginSuccess(loginResult, loginType),
+            (playFabError) => OnLoginError(playFabError, loginType));
     }
 
-    void OnCustomIDLoginError(PlayFabError playFabError)
+    public void AddUsernamePassword(string email, string password, string username)
     {
-        Debug.Log("Error while logging in/creating Custom ID Account");
+        var request = new AddUsernamePasswordRequest
+        {
+            Email = email,
+            Password = password,
+            Username = username
+        };
+        PlayFabClientAPI.AddUsernamePassword(
+            request,
+            (aupResult) => OnAUPSuccess(aupResult),
+            (playFabError) => OnAUPError(playFabError));
+    }
+
+    public void Logout()
+    {
+        PlayFabClientAPI.ForgetAllCredentials();
+    }
+
+    void OnLoginSuccess(LoginResult loginResult, LoginType loginType)
+    {
+        Debug.Log(loginType.ToString() + " Account created successfully");
+        SessionTicket = loginResult.SessionTicket;
+    }
+
+    void OnLoginError(PlayFabError playFabError, LoginType loginType)
+    {
+        Debug.Log("Error while logging in/creating " + loginType.ToString() + " Account");
         Debug.Log(playFabError.GenerateErrorReport());
     }
 
+    void OnAUPSuccess(AddUsernamePasswordResult aupResult)
+    {
+        Debug.Log("AddUsernamePassword successful: Welcome " + aupResult.Username);
+    }
+
+    void OnAUPError(PlayFabError playFabError)
+    {
+        Debug.Log("Error while adding username and password");
+        Debug.Log(playFabError.GenerateErrorReport());
+    }
+}
+
+public enum LoginType
+{
+    CustomID,
+    EmailAddress
 }
