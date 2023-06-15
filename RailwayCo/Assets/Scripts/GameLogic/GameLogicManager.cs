b0e15b5c439d32b88eece571b8b9efd870feb26c
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-06-16 02:03:43 +0800

    fix: missing initializer for playfabmanager

--- RailwayCo/Assets/Scripts/GameLogic/GameLogicManager.cs
+++ RailwayCo/Assets/Scripts/GameLogic/GameLogicManager.cs
@@ -1,3 +1,4 @@
+using PlayFab;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "GamelogicScriptableObject", menuName = "ScriptableObjects/Gamelogic")]
@@ -15,5 +16,26 @@ public class GameLogicManager : ScriptableObject
     {
         User = user;
         StationMaster = stationMaster;
+    }
+
+    private void OnEnable()
+    {
+#if UNITY_EDITOR
+        // Source: https://forum.unity.com/threads/solved-but-unhappy-scriptableobject-awake-never-execute.488468/#post-5564170
+        // use platform dependent compilation so it only exists in editor, otherwise it'll break the build
+        if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
+            Init();
+#endif       
+    }
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        PlayFabSettings.TitleId = "357DE";
+        PlayfabManager = new();
     }
 }
