17e056d2bf5f0704dc53ed6cca483c19b5cef401
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-06-18 22:05:05 +0800

    feat: added methods for StationMaster
    Can be utilised in GameManager

--- RailwayCo/Assets/Scripts/GameLogic/Station/StationMaster.cs
+++ RailwayCo/Assets/Scripts/GameLogic/Station/StationMaster.cs
@@ -15,11 +15,15 @@ public class StationMaster : Master<Station>
         HashSet<Guid> tracksToRemove = stationToRemove.StationHelper.GetAll();
         foreach(Guid _guid in tracksToRemove)
         {
-            Station relatedStation = Get(_guid);
-            relatedStation.StationHelper.Remove(guid);
+            RemoveTrack(guid, _guid);
         }
         Remove(guid);
     }
+
+    public void LockStation(Guid guid) => Get(guid).Lock();
+    public void UnlockStation(Guid guid) => Get(guid).Unlock();
+    public void OpenStation(Guid guid) => Get(guid).Open();
+    public void CloseStation(Guid guid) => Get(guid).Close();
 
     public void AddTrack(Guid guid1, Guid guid2)
     {
@@ -35,5 +39,25 @@ public class StationMaster : Master<Station>
         Station station2 = Get(guid2);
         station1.StationHelper.Remove(guid2);
         station2.StationHelper.Remove(guid1);
+    }
+
+    public void AddTrain(Guid stationGuid, Guid trainGuid)
+    {
+        Get(stationGuid).TrainHelper.Add(trainGuid);
+    }
+
+    public void RemoveTrain(Guid stationGuid, Guid trainGuid)
+    {
+        Get(stationGuid).TrainHelper.Remove(trainGuid);
+    }
+
+    public void AddCargo(Guid stationGuid, Guid cargoGuid)
+    {
+        Get(stationGuid).CargoHelper.Add(cargoGuid);
+    }
+
+    public void RemoveCargo(Guid stationGuid, Guid cargoGuid)
+    {
+        Get(stationGuid).CargoHelper.Remove(cargoGuid);
     }
 }
