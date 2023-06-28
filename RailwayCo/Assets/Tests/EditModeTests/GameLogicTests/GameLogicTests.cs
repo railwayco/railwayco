ca7cd2ce2087e13eac1cb37aa1e31b40515f8c21
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-06-28 23:42:17 +0800

    test: remove old methods no longer used

--- RailwayCo/Assets/Tests/EditModeTests/GameLogicTests/GameLogicTests.cs
+++ RailwayCo/Assets/Tests/EditModeTests/GameLogicTests/GameLogicTests.cs
@@ -6,36 +6,6 @@ using UnityEngine;
 
 public class GameLogicTests
 {
-    // User related tests
-    [Test]
-    public void GameLogic_GetUserName_UserNameNotNull()
-    {
-        GameLogic gameLogic = GameLogicInit();
-        Assert.DoesNotThrow(() => gameLogic.GetUserName());
-    }
-
-    [Test]
-    public void GameLogic_GetUserExperiencePoints_ExperiencePointsNotNull()
-    {
-        GameLogic gameLogic = GameLogicInit();
-        Assert.DoesNotThrow(() => gameLogic.GetUserExperiencePoints());
-    }
-
-    [Test]
-    public void GameLogic_GetUserSkillPoints_SkillPointsNotNull()
-    {
-        GameLogic gameLogic = GameLogicInit();
-        Assert.DoesNotThrow(() => gameLogic.GetUserSkillPoints());
-    }
-
-    [Test]
-    public void GameLogic_GetUserCurrencyManager_CurrencyManagerNotNull()
-    {
-        GameLogic gameLogic = GameLogicInit();
-        Assert.DoesNotThrow(() => gameLogic.GetUserCurrencyManager());
-    }
-
-
     // CargoModel related tests
     [Test]
     public void GameLogic_GetRandomCargoModel_IsRandom()
@@ -54,71 +24,52 @@ public class GameLogicTests
         Assert.That(cargoModels.Count(c => c.Guid == guid), Is.LessThan(numOfCargoModels));
     }
 
-    [Test]
-    public void GameLogic_AddCargoModel_CargoModelIsAdded()
-    {
-        GameLogic gameLogic = GameLogicInit();
-        CargoModel cargoModel = new(CargoType.Wood, 50.0, 100.0, new());
-        gameLogic.AddCargoModel(cargoModel);
-        Assert.DoesNotThrow(() => gameLogic.GetCargoModelRef(cargoModel.Guid));
-    }
-
-    [Test]
-    public void GameLogic_RemoveCargoModel_CargoModelIsRemoved()
-    {
-        GameLogic gameLogic = GameLogicInit();
-        CargoModel cargoModel = new(CargoType.Wood, 50.0, 100.0, new());
-        gameLogic.AddCargoModel(cargoModel);
-        gameLogic.RemoveCargoModel(cargoModel.Guid);
-        Assert.Throws<NullReferenceException>(() => gameLogic.GetCargoModelRef(cargoModel.Guid));
-    }
-
     // Station related tests
     [Test]
     public void GameLogic_InitStation_IsStationAdded()
     {
         GameLogic gameLogic = GameLogicInit();
         Guid stationGuid = gameLogic.InitStation("Station1", new());
-        Assert.DoesNotThrow(() => gameLogic.GetStationRef(stationGuid));
+        Assert.DoesNotThrow(() => gameLogic.StationMaster.GetRef(stationGuid));
     }
 
     [Test]
     public void GameLogic_AddRandomCargoToStation_StationIsNotEmpty()
     {
         GameLogic gameLogic = GameLogicWithStationsInit();
-        Guid stationGuid = gameLogic.GetAllStationGuids().ToList()[0];
+        Guid stationGuid = gameLogic.StationMaster.GetAll().ToList()[0];
         int numberOfNewCargo = 10;
 
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromStation(stationGuid).Count == 0);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().Count == 0);
         gameLogic.AddRandomCargoToStation(stationGuid, numberOfNewCargo);
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromStation(stationGuid).Count == numberOfNewCargo);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().Count == numberOfNewCargo);
     }
 
     [TestCase(StationOrientation.Head_Head, ExpectedResult = StationOrientation.Head_Head)]
     [TestCase(StationOrientation.Head_Tail, ExpectedResult = StationOrientation.Tail_Head)]
     [TestCase(StationOrientation.Tail_Tail, ExpectedResult = StationOrientation.Tail_Tail)]
     [TestCase(StationOrientation.Tail_Head, ExpectedResult = StationOrientation.Head_Tail)]
-    public StationOrientation GameLogic_AddStationToStation_StationsAreLinkedCorrectly(StationOrientation orientation)
+    public StationOrientation GameLogic_AddTrack_StationsAreLinkedCorrectly(StationOrientation orientation)
     {
         GameLogic gameLogic = GameLogicWithStationsInit();
-        Guid stationGuid = gameLogic.GetAllStationGuids().ToList()[0];
+        Guid stationGuid = gameLogic.StationMaster.GetAll().ToList()[0];
         Guid newStationGuid = gameLogic.InitStation("Station3", new());
-        gameLogic.AddStationToStation(stationGuid, newStationGuid, orientation);
-        return gameLogic.GetStationRef(newStationGuid).StationHelper.GetObject(stationGuid);
+        gameLogic.AddTrack(stationGuid, newStationGuid, orientation);
+        return gameLogic.StationMaster.GetRef(newStationGuid).StationHelper.GetObject(stationGuid);
     }
     
     [Test]
     public void GameLogic_RemoveStationFromStation_StationLinksAreRemovedCorrectly()
     {
         GameLogic gameLogic = GameLogicWithStationsInit();
-        Guid station1Guid = gameLogic.GetAllStationGuids().ToList()[0];
-        Guid station2Guid = gameLogic.GetAllStationGuids().ToList()[1];
+        Guid station1Guid = gameLogic.StationMaster.GetAll().ToList()[0];
+        Guid station2Guid = gameLogic.StationMaster.GetAll().ToList()[1];
 
-        Assert.DoesNotThrow(() => gameLogic.GetStationRef(station1Guid).StationHelper.GetObject(station2Guid));
-        Assert.DoesNotThrow(() => gameLogic.GetStationRef(station1Guid).StationHelper.GetObject(station2Guid));
-        gameLogic.RemoveStationFromStation(station1Guid, station2Guid);
-        Assert.IsTrue(gameLogic.GetStationRef(station1Guid).StationHelper.GetObject(station2Guid) == default);
-        Assert.IsTrue(gameLogic.GetStationRef(station1Guid).StationHelper.GetObject(station2Guid) == default);
+        Assert.DoesNotThrow(() => gameLogic.StationMaster.GetRef(station1Guid).StationHelper.GetObject(station2Guid));
+        Assert.DoesNotThrow(() => gameLogic.StationMaster.GetRef(station1Guid).StationHelper.GetObject(station2Guid));
+        gameLogic.RemoveTrack(station1Guid, station2Guid);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(station1Guid).StationHelper.GetObject(station2Guid) == default);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(station1Guid).StationHelper.GetObject(station2Guid) == default);
     }
 
     [TestCase(1F, 2F, 3F)]
@@ -127,13 +78,13 @@ public class GameLogicTests
     {
         GameLogic gameLogic = GameLogicInit();
         Guid stationGuid = gameLogic.InitStation("Station1", new());
-        Station station = gameLogic.GetStationRef(stationGuid);
+        Station station = gameLogic.StationMaster.GetObject(stationGuid);
         Vector3 vector = new(x, y, z);
 
         Assert.AreEqual(station.Attribute.Position, new Vector3());
-        gameLogic.SetStationUnityStats(stationGuid, vector);
+        station.Attribute.SetUnityStats(vector);
 
-        station = gameLogic.GetStationRef(stationGuid);
+        station = gameLogic.StationMaster.GetRef(stationGuid);
         Assert.AreEqual(station.Attribute.Position, vector);
     }
 
@@ -149,105 +100,102 @@ public class GameLogicTests
         Assert.AreEqual(stationGuid, station.Guid);
     }
 
+    [Test]
+    public void GameLogic_AddCargoToStation_CargoAddedToStation()
+    {
+        GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
+        Guid stationGuid = gameLogic.StationMaster.GetAll().ToList()[0];
+        Guid trainGuid = gameLogic.TrainMaster.GetAll().ToList()[0];
+        gameLogic.AddRandomCargoToStation(stationGuid, 10);
+        Guid cargoGuid = gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().ToList()[0];
+        gameLogic.RemoveCargoFromStation(stationGuid, cargoGuid);
+        gameLogic.AddCargoToTrain(trainGuid, cargoGuid);
+
+        Assert.IsFalse(gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().Contains(cargoGuid));
+        gameLogic.AddCargoToStation(stationGuid, cargoGuid);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().Contains(cargoGuid));
+    }
+
+    [Test]
+    public void GameLogic_RemoveCargoFromStation_CargoRemovedFromStation()
+    {
+        GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
+        Guid stationGuid = gameLogic.StationMaster.GetAll().ToList()[0];
+        gameLogic.AddRandomCargoToStation(stationGuid, 10);
+        Guid cargoGuid = gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().ToList()[0];
+
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().Contains(cargoGuid));
+        gameLogic.RemoveCargoFromStation(stationGuid, cargoGuid);
+        Assert.IsFalse(gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().Contains(cargoGuid));
+    }
+
     // Train related tests
     [Test]
     public void GameLogic_InitTrain_IsTrainAdded()
     {
         GameLogic gameLogic = GameLogicInit();
         Guid trainGuid = gameLogic.InitTrain("Train1", 5.0, new(), new(), TrainDirection.NORTH);
-        Assert.DoesNotThrow(() => gameLogic.GetTrainRef(trainGuid));
+        Assert.DoesNotThrow(() => gameLogic.TrainMaster.GetRef(trainGuid));
     }
 
     [Test]
     public void GameLogic_SetTrainTravelPlan_SourceAndDestinationStationsAreCorrect()
     {
         GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
-        Guid station1Guid = gameLogic.GetAllStationGuids().ToList()[0];
-        Guid station2Guid = gameLogic.GetAllStationGuids().ToList()[1];
-        Guid trainGuid = gameLogic.GetAllTrainGuids().ToList()[0];
+        Guid station1Guid = gameLogic.StationMaster.GetAll().ToList()[0];
+        Guid station2Guid = gameLogic.StationMaster.GetAll().ToList()[1];
+        Guid trainGuid = gameLogic.TrainMaster.GetAll().ToList()[0];
 
         gameLogic.SetTrainTravelPlan(trainGuid, station1Guid, station2Guid);
-        Train train = gameLogic.GetTrainRef(trainGuid);
+        Train train = gameLogic.TrainMaster.GetRef(trainGuid);
         Assert.AreEqual(station1Guid, train.TravelPlan.SourceStation);
         Assert.AreEqual(station2Guid, train.TravelPlan.DestinationStation);
-    }
-    
-    [Test]
-    public void GameLogic_GetTrainDestination_IsTrainDestination()
-    {
-        GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
-        Guid station1Guid = gameLogic.GetAllStationGuids().ToList()[0];
-        Guid station2Guid = gameLogic.GetAllStationGuids().ToList()[1];
-        Guid trainGuid = gameLogic.GetAllTrainGuids().ToList()[0];
-        gameLogic.SetTrainTravelPlan(trainGuid, station1Guid, station2Guid);
-
-        Train train = gameLogic.GetTrainRef(trainGuid);
-        Assert.AreEqual(gameLogic.GetTrainDestination(trainGuid), train.TravelPlan.DestinationStation);
     }
 
     [Test]
     public void GameLogic_OnTrainArrival_IsExpectedBehaviour()
     {
         GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
-        Guid station1Guid = gameLogic.GetAllStationGuids().ToList()[0];
-        Guid station2Guid = gameLogic.GetAllStationGuids().ToList()[1];
-        Guid trainGuid = gameLogic.GetAllTrainGuids().ToList()[0];
+        Guid station1Guid = gameLogic.StationMaster.GetAll().ToList()[0];
+        Guid station2Guid = gameLogic.StationMaster.GetAll().ToList()[1];
+        Guid trainGuid = gameLogic.TrainMaster.GetAll().ToList()[0];
         gameLogic.SetTrainTravelPlan(trainGuid, station1Guid, station2Guid);
-        Assert.IsTrue(gameLogic.GetAllCargoGuids().Count == 0);
+        Assert.IsTrue(gameLogic.CargoMaster.GetAll().Count == 0);
 
         gameLogic.AddRandomCargoToStation(station1Guid, 10);
-        Assert.IsFalse(gameLogic.GetAllCargoGuids().Count == 0);
+        Assert.IsFalse(gameLogic.CargoMaster.GetAll().Count == 0);
 
-        HashSet<Guid> guids = gameLogic.GetAllCargoGuidsFromStation(station1Guid);
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromTrain(trainGuid).Count == 0);
-        
-        foreach (var guid in guids) gameLogic.MoveCargoFromStationToTrain(guid, station1Guid, trainGuid);
-        Assert.IsFalse(gameLogic.GetAllCargoGuidsFromTrain(trainGuid).Count == 0);
+        HashSet<Guid> guids = gameLogic.StationMaster.GetRef(station1Guid).CargoHelper.GetAll();
+        Assert.IsTrue(gameLogic.TrainMaster.GetRef(trainGuid).CargoHelper.GetAll().Count == 0);
 
-        Assert.IsTrue(gameLogic.GetStationRef(station2Guid).TrainHelper.GetAll().Count == 0);
+        foreach (var guid in guids)
+        {
+            gameLogic.RemoveCargoFromStation(station1Guid, guid);
+            gameLogic.AddCargoToTrain(trainGuid, guid);
+        }
+        Assert.IsFalse(gameLogic.TrainMaster.GetRef(trainGuid).CargoHelper.GetAll().Count == 0);
+
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(station2Guid).TrainHelper.GetAll().Count == 0);
         gameLogic.OnTrainArrival(trainGuid);
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromTrain(trainGuid).Count == 0);
-        Assert.IsTrue(gameLogic.GetAllCargoGuids().Count == 0);
-        Assert.IsTrue(gameLogic.GetStationRef(station2Guid).TrainHelper.GetAll().Count == 1);
+        Assert.IsTrue(gameLogic.TrainMaster.GetRef(trainGuid).CargoHelper.GetAll().Count == 0);
+        Assert.IsTrue(gameLogic.CargoMaster.GetAll().Count == 0);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(station2Guid).TrainHelper.GetAll().Count == 1);
     }
     
     [Test]
     public void GameLogic_OnTrainDeparture_IsExpectedBehaviour()
     {
         GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
-        Guid station1Guid = gameLogic.GetAllStationGuids().ToList()[0];
-        Guid station2Guid = gameLogic.GetAllStationGuids().ToList()[1];
-        Guid trainGuid = gameLogic.GetAllTrainGuids().ToList()[0];
+        Guid station1Guid = gameLogic.StationMaster.GetAll().ToList()[0];
+        Guid station2Guid = gameLogic.StationMaster.GetAll().ToList()[1];
+        Guid trainGuid = gameLogic.TrainMaster.GetAll().ToList()[0];
         gameLogic.SetTrainTravelPlan(trainGuid, station2Guid, station1Guid);
         gameLogic.OnTrainArrival(trainGuid);
-        Assert.IsTrue(gameLogic.GetStationRef(station1Guid).TrainHelper.GetAll().Count == 1);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(station1Guid).TrainHelper.GetAll().Count == 1);
 
         gameLogic.OnTrainDeparture(trainGuid, station1Guid, station2Guid);
-        Assert.IsTrue(gameLogic.GetTrainDestination(trainGuid) == station2Guid);
-        Assert.IsTrue(gameLogic.GetStationRef(station1Guid).TrainHelper.GetAll().Count == 0);
-    }
-
-    [TestCase(1F, 2F, 3F, 4F)]
-    [TestCase(-1F, 2.3F, -3.5F, 5F)]
-    public void GameLogic_SetTrainUnityStats_TrainUnityStatsCorrect(float x, float y, float z, float w)
-    {
-        GameLogic gameLogic = GameLogicInit();
-        Guid trainGuid = gameLogic.InitTrain("Train1", 5.0, new(), new(), TrainDirection.NORTH);
-        Train train = gameLogic.GetTrainRef(trainGuid);
-        Vector3 vector = new(x, y, z);
-        Quaternion quaternion = new(x, y, z, w);
-
-        Assert.AreEqual(0.0, train.Attribute.Speed.Amount);
-        Assert.AreEqual(new Vector3(), train.Attribute.Position);
-        Assert.AreEqual(new Quaternion(), train.Attribute.Rotation);
-        Assert.AreEqual(TrainDirection.NORTH, train.Attribute.Direction);
-        gameLogic.SetTrainUnityStats(trainGuid, x, vector, quaternion, TrainDirection.SOUTH);
-
-        train = gameLogic.GetTrainRef(trainGuid);
-        Assert.AreEqual(x, train.Attribute.Speed.Amount);
-        Assert.AreEqual(vector, train.Attribute.Position);
-        Assert.AreEqual(quaternion, train.Attribute.Rotation);
-        Assert.AreEqual(TrainDirection.SOUTH, train.Attribute.Direction);
+        Assert.IsTrue(gameLogic.TrainMaster.GetRef(trainGuid).TravelPlan.DestinationStation == station2Guid);
+        Assert.IsTrue(gameLogic.StationMaster.GetRef(station1Guid).TrainHelper.GetAll().Count == 0);
     }
 
     [TestCase(1F, 2F, 3F)]
@@ -262,38 +210,34 @@ public class GameLogicTests
         Assert.AreEqual(trainGuid, train.Guid);
     }
 
-    // Hybrid tests
     [Test]
-    public void GameLogic_MoveCargoFromStationToTrain_CargoMovedFromStationToTrain()
+    public void GameLogic_AddCargoToTrain_CargoAddedToTrain()
     {
         GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
-        Guid stationGuid = gameLogic.GetAllStationGuids().ToList()[0];
-        Guid trainGuid = gameLogic.GetAllTrainGuids().ToList()[0];
+        Guid stationGuid = gameLogic.StationMaster.GetAll().ToList()[0];
+        Guid trainGuid = gameLogic.TrainMaster.GetAll().ToList()[0];
         gameLogic.AddRandomCargoToStation(stationGuid, 10);
-        Guid cargoGuid = gameLogic.GetAllCargoGuidsFromStation(stationGuid).ToList()[0];
+        Guid cargoGuid = gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().ToList()[0];
 
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromStation(stationGuid).Contains(cargoGuid));
-        Assert.IsFalse(gameLogic.GetAllCargoGuidsFromTrain(trainGuid).Contains(cargoGuid));
-        gameLogic.MoveCargoFromStationToTrain(cargoGuid, stationGuid, trainGuid);
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromTrain(trainGuid).Contains(cargoGuid));
-        Assert.IsFalse(gameLogic.GetAllCargoGuidsFromStation(stationGuid).Contains(cargoGuid));
+        Assert.IsFalse(gameLogic.TrainMaster.GetRef(trainGuid).CargoHelper.GetAll().Contains(cargoGuid));
+        gameLogic.AddCargoToTrain(trainGuid, cargoGuid);
+        Assert.IsTrue(gameLogic.TrainMaster.GetRef(trainGuid).CargoHelper.GetAll().Contains(cargoGuid));
     }
 
     [Test]
-    public void GameLogic_MoveCargoFromTrainToStation_CargoMovedFromTrainToStation()
+    public void GameLogic_RemoveCargoFromTrain_CargoRemovedFromTrain()
     {
         GameLogic gameLogic = GameLogicWithStationsAndTrainInit();
-        Guid stationGuid = gameLogic.GetAllStationGuids().ToList()[0];
-        Guid trainGuid = gameLogic.GetAllTrainGuids().ToList()[0];
+        Guid stationGuid = gameLogic.StationMaster.GetAll().ToList()[0];
+        Guid trainGuid = gameLogic.TrainMaster.GetAll().ToList()[0];
         gameLogic.AddRandomCargoToStation(stationGuid, 10);
-        Guid cargoGuid = gameLogic.GetAllCargoGuidsFromStation(stationGuid).ToList()[0];
-        gameLogic.MoveCargoFromStationToTrain(cargoGuid, stationGuid, trainGuid);
+        Guid cargoGuid = gameLogic.StationMaster.GetRef(stationGuid).CargoHelper.GetAll().ToList()[0];
+        gameLogic.RemoveCargoFromStation(stationGuid, cargoGuid);
+        gameLogic.AddCargoToTrain(trainGuid, cargoGuid);
 
-        Assert.IsFalse(gameLogic.GetAllCargoGuidsFromStation(stationGuid).Contains(cargoGuid));
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromTrain(trainGuid).Contains(cargoGuid));
-        gameLogic.MoveCargoFromTrainToStation(cargoGuid, stationGuid, trainGuid);
-        Assert.IsFalse(gameLogic.GetAllCargoGuidsFromTrain(trainGuid).Contains(cargoGuid));
-        Assert.IsTrue(gameLogic.GetAllCargoGuidsFromStation(stationGuid).Contains(cargoGuid));
+        Assert.IsTrue(gameLogic.TrainMaster.GetRef(trainGuid).CargoHelper.GetAll().Contains(cargoGuid));
+        gameLogic.RemoveCargoFromTrain(trainGuid, cargoGuid);
+        Assert.IsFalse(gameLogic.TrainMaster.GetRef(trainGuid).CargoHelper.GetAll().Contains(cargoGuid));
     }
 
 
@@ -302,7 +246,7 @@ public class GameLogicTests
     private GameLogic GameLogicWithCargoModelInit()
     {
         GameLogic gameLogic = GameLogicInit();
-        gameLogic.GenerateRandomData();
+        gameLogic.GenerateCargoModels();
         return gameLogic;
     }
     private GameLogic GameLogicWithStationsInit()
@@ -310,7 +254,7 @@ public class GameLogicTests
         GameLogic gameLogic = GameLogicWithCargoModelInit();
         Guid station1Guid = gameLogic.InitStation("Station1", new());
         Guid station2Guid = gameLogic.InitStation("Station2", new());
-        gameLogic.AddStationToStation(station1Guid, station2Guid, StationOrientation.Head_Tail);
+        gameLogic.AddTrack(station1Guid, station2Guid, StationOrientation.Head_Tail);
         return gameLogic;
     }
     private GameLogic GameLogicWithStationsAndTrainInit()
