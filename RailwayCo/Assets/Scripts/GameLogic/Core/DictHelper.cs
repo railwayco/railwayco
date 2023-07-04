16cf74f24ec237894529fe58b56f222988a10390
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-05 00:36:06 +0800

    feat: add ReaderWriterLock for read write control

--- RailwayCo/Assets/Scripts/GameLogic/Core/DictHelper.cs
+++ RailwayCo/Assets/Scripts/GameLogic/Core/DictHelper.cs
@@ -1,37 +1,80 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
 
 public class DictHelper<T> : ICloneable
 {
     protected Dictionary<Guid, T> Collection { get; set; }
+    protected ReaderWriterLock ReaderWriterLock { get; set; }
 
-    public DictHelper() => Collection = new();
+    public DictHelper()
+    {
+        Collection = new();
+        ReaderWriterLock = new();
+    }
 
-    public int Count() => Collection.Count;
+    public void AcquireReaderLock(int milisecTimeout = Timeout.Infinite) => ReaderWriterLock.AcquireReaderLock(milisecTimeout);
+    public void ReleaseReaderLock() => ReaderWriterLock.ReleaseReaderLock();
+    public void AcquireWriterLock(int milisecTimeout = Timeout.Infinite) => ReaderWriterLock.AcquireWriterLock(milisecTimeout);
+    public void ReleaseWriterLock() => ReaderWriterLock.ReleaseWriterLock();
 
-    public void Add(Guid guid, T item) => Collection.Add(guid, item);
+    public int Count()
+    {
+        AcquireReaderLock();
+        int count = Collection.Count;
+        ReleaseReaderLock();
+        return count;
+    }
 
-    public void Remove(Guid guid) => Collection.Remove(guid);
+    public void Add(Guid guid, T item)
+    {
+        AcquireWriterLock(Timeout.Infinite);
+        Collection.Add(guid, item);
+        ReleaseWriterLock();
+    }
+
+    public void Remove(Guid guid)
+    {
+        AcquireWriterLock(Timeout.Infinite);
+        Collection.Remove(guid);
+        ReleaseWriterLock();
+    }
 
     public void Update(Guid guid, T value)
     {
+        AcquireWriterLock(Timeout.Infinite);
         if (Collection.ContainsKey(guid)) Collection[guid] = value;
+        ReleaseWriterLock();
     }
 
+    /// <summary>
+    /// Get the raw object based on requested guid. Need to AcquireWriterLock()
+    /// from ReaderWriterLock and ReleaseWriterLock() when done.
+    /// </summary>
     public T GetObject(Guid guid)
     {
         if (!Collection.ContainsKey(guid)) return default;
         return Collection[guid];
     }
 
-    public HashSet<Guid> GetAll() => new(Collection.Keys);
+    public HashSet<Guid> GetAll()
+    {
+        AcquireReaderLock(Timeout.Infinite);
+        HashSet<Guid> guids = new(Collection.Keys);
+        ReleaseReaderLock();
+        return guids;
+    }
 
     /// <summary>Clones Collection, given that T is of value type. If T is of reference type, 
     /// override this method and write a new implementation of Clone.</summary>
     public virtual object Clone()
     {
+        AcquireReaderLock(Timeout.Infinite);
         DictHelper<T> dictHelper = (DictHelper<T>)MemberwiseClone();
+        ReleaseReaderLock();
+
         dictHelper.Collection = new(dictHelper.Collection);
+
         return dictHelper;
     }
 }
--- RailwayCo/Assets/Scripts/GameLogic/Core/WorkerDictHelper.cs
+++ RailwayCo/Assets/Scripts/GameLogic/Core/WorkerDictHelper.cs
@@ -1,10 +1,26 @@
 using System;
+using System.Threading;
 
 public class WorkerDictHelper<T> : DictHelper<T> where T : Worker
 {
-    public WorkerDictHelper() => Collection = new();
+    public WorkerDictHelper()
+    {
+        Collection = new();
+        ReaderWriterLock = new();
+    }
 
-    public void Add(T item) => Collection.Add(item.Guid, item);
+    public void Add(T item)
+    {
+        AcquireWriterLock(Timeout.Infinite);
+        Collection.Add(item.Guid, item);
+        ReleaseWriterLock();
+    }
 
-    public T GetRef(Guid guid) => (T)GetObject(guid).Clone();
+    public T GetRef(Guid guid)
+    {
+        AcquireReaderLock(Timeout.Infinite);
+        T tObject = (T)GetObject(guid).Clone();
+        ReleaseReaderLock();
+        return tObject;
+    }
 }
