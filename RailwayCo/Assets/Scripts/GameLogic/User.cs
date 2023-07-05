e76bf8ddeb5bc4fae8e5a948840f2cd0e9575445
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-05 18:11:17 +0800

    feat: add backing field and read write lock
    lock added to properties

--- RailwayCo/Assets/Scripts/GameLogic/User.cs
+++ RailwayCo/Assets/Scripts/GameLogic/User.cs
@@ -5,14 +5,73 @@ public class User
 {
     public IThreadLock RWLock { get; }
     private CurrencyManager _currencyManager;
+    private string _name;
+    private int _experiencePoint;
+    private int _skillPoint;
 
-    public string Name { get; private set; }
-    public int ExperiencePoint { get; private set; }
-    public int SkillPoint { get; private set; }
-    public CurrencyManager CurrencyManager 
-    { 
-        get => (CurrencyManager)_currencyManager.Clone();
-        private set => _currencyManager = value;
+    public string Name 
+    {
+        get 
+        {
+            RWLock.AcquireReaderLock();
+            string name = _name;
+            RWLock.ReleaseReaderLock();
+            return name;
+        }
+        private set
+        {
+            RWLock.AcquireWriterLock();
+            _name = value;
+            RWLock.ReleaseWriterLock();
+        } 
+    }
+    public int ExperiencePoint
+    {
+        get
+        {
+            RWLock.AcquireReaderLock();
+            int experiencePoint = _experiencePoint;
+            RWLock.ReleaseReaderLock();
+            return experiencePoint;
+        }
+        private set
+        {
+            RWLock.AcquireWriterLock();
+            _experiencePoint = value;
+            RWLock.ReleaseWriterLock();
+        }
+    }
+    public int SkillPoint
+    {
+        get
+        {
+            RWLock.AcquireReaderLock();
+            int skillPoint = _skillPoint;
+            RWLock.ReleaseReaderLock();
+            return skillPoint;
+        }
+        private set
+        {
+            RWLock.AcquireWriterLock();
+            _skillPoint = value;
+            RWLock.ReleaseWriterLock();
+        }
+    }
+    public CurrencyManager CurrencyManager
+    {
+        get
+        {
+            RWLock.AcquireReaderLock();
+            CurrencyManager currencyManager = (CurrencyManager)_currencyManager.Clone();
+            RWLock.ReleaseReaderLock();
+            return currencyManager;
+        }
+        private set
+        {
+            RWLock.AcquireWriterLock();
+            _currencyManager = value;
+            RWLock.ReleaseWriterLock();
+        }
     }
 
     public User(string name, int experiencePoint, int skillPoint, CurrencyManager currencyManager)
@@ -28,30 +87,24 @@ public class User
     public void AddExperiencePoint(int experiencePoint)
     {
         if (experiencePoint < 0) throw new ArgumentException("Invalid experience points");
-        RWLock.AcquireWriterLock(Timeout.Infinite);
         ExperiencePoint = Arithmetic.IntAddition(ExperiencePoint, experiencePoint);
-        RWLock.ReleaseWriterLock();
     }
 
     public void AddSkillPoint(int skillPoint)
     {
         if (skillPoint < 0) throw new ArgumentException("Invalid skill points");
-        RWLock.AcquireWriterLock(Timeout.Infinite);
         SkillPoint = Arithmetic.IntAddition(SkillPoint, skillPoint);
-        RWLock.ReleaseWriterLock();
     }
 
     public void RemoveSkillPoint(int skillPoint)
     {
         if (skillPoint < 0) throw new ArgumentException("Invalid skill points");
-        RWLock.AcquireWriterLock(Timeout.Infinite);
         SkillPoint = Arithmetic.IntSubtraction(SkillPoint, skillPoint);
-        RWLock.ReleaseWriterLock();
     }
 
     public void AddCurrencyManager(CurrencyManager currencyManager)
     {
-        RWLock.AcquireWriterLock(Timeout.Infinite);
+        RWLock.AcquireWriterLock();
         CurrencyManager.AddCurrencyManager(currencyManager);
         RWLock.ReleaseWriterLock();
     }
