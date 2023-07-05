3541297723b11d1e3c187fec41ad0f03f5676db3
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-05 15:36:45 +0800

    feat: added IThreadLock interface

---
+++ RailwayCo/Assets/Scripts/GameLogic/Core/ThreadLock/IThreadLock.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading;
+
+public interface IThreadLock
+{
+    public ReaderWriterLock ReaderWriterLock { get; protected set; }
+
+    void AcquireReaderLock(int milisecTimeout = Timeout.Infinite) => ReaderWriterLock.AcquireReaderLock(milisecTimeout);
+    void ReleaseReaderLock() => ReaderWriterLock.ReleaseReaderLock();
+    void AcquireWriterLock(int milisecTimeout = Timeout.Infinite) => ReaderWriterLock.AcquireWriterLock(milisecTimeout);
+    void ReleaseWriterLock() => ReaderWriterLock.ReleaseWriterLock();
+}
---
+++ RailwayCo/Assets/Scripts/GameLogic/Core/ThreadLock/RWLock.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+public class RWLock : IThreadLock
+{
+    private ReaderWriterLock _readerWriterLock;
+    ReaderWriterLock IThreadLock.ReaderWriterLock 
+    { 
+        get => _readerWriterLock; 
+        set => _readerWriterLock = value;
+    }
+
+    public RWLock() => _readerWriterLock = new();
+}
