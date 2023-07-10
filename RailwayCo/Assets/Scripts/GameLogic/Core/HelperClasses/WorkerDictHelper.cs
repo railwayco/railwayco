82ec24d5f5c581df35abe77253097912c2919d31
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-10 19:18:57 +0800

    refactor: remove read write lock feature
    playfab sdk uses MonoBehaviour and cannot be executed in a separate thread from Unity main thread

--- RailwayCo/Assets/Scripts/GameLogic/Core/HelperClasses/WorkerDictHelper.cs
+++ RailwayCo/Assets/Scripts/GameLogic/Core/HelperClasses/WorkerDictHelper.cs
@@ -1,22 +1,10 @@
 using System;
-using System.Threading;
 
 public class WorkerDictHelper<T> : DictHelper<T> where T : Worker
 {
     public WorkerDictHelper() : base() { }
 
-    public void Add(T item)
-    {
-        RWLock.AcquireWriterLock(Timeout.Infinite);
-        Collection.Add(item.Guid, item);
-        RWLock.ReleaseWriterLock();
-    }
+    public void Add(T item) => Collection.Add(item.Guid, item);
 
-    public T GetRef(Guid guid)
-    {
-        RWLock.AcquireReaderLock(Timeout.Infinite);
-        T tObject = (T)GetObject(guid).Clone();
-        RWLock.ReleaseReaderLock();
-        return tObject;
-    }
+    public T GetRef(Guid guid) => (T)GetObject(guid).Clone();
 }
