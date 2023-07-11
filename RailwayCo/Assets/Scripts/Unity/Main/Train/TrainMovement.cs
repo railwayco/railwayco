dc94ed2518de62069c81a5d8815bf29f37c87f56
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2023-07-11 21:33:08 +0800

    feat: add incline movement logic
    Minor changes to prefabs. Also included scene to try it out.
    Note the workaround gor the LogicManager

--- RailwayCo/Assets/Scripts/Unity/Main/Train/TrainMovement.cs
+++ RailwayCo/Assets/Scripts/Unity/Main/Train/TrainMovement.cs
@@ -19,12 +19,15 @@ public class TrainMovement : MonoBehaviour
     public TrainDirection MovementDirn { get; private set; }
 
     private TrackType _trackType;
+    private TrackType _prevTrackType;
     private TrainState _trainState;
     private List<Transform> _waypointPath;
+    private Collider _collidedObject;
 
     // The 4 kinds of curved tracks and the straights
     private enum TrackType
     {
+        Nil,
         RightUp,
         RightDown,
         LeftUp,
@@ -111,39 +114,71 @@ public class TrainMovement : MonoBehaviour
     /// TRAIN MOVEMENT DETERMINATION LOGIC (STATION_DEPART-ed)
     //////////////////////////////////////////////////////
 
-    private void OnTriggerEnter(Collider other)
+    private IEnumerator OnTriggerEnter(Collider other)
     {
+
         // Due to the introduction of 2 Box colliders for the curved trakcks,
         // we need to check for and ignore the second box collider and not proceed with further processing.
-        if (IsStillCurvedTrack(other.tag, _trackType)) return;
+        _collidedObject = other;
+        if (IsStillCurvedTrack(other.tag, _trackType)) yield break;
 
         _waypointPath = GetWaypoints(other);
         
-        Debug.LogError(other.tag);
+        Debug.LogWarning(other.tag);
+        _prevTrackType = _trackType;
+
         // Sets the relevant flags so that the MoveTrain function will know how to divert code execution
         switch (other.tag)
         {
             case "PlatformTD":
             case "PlatformLR":
                 _trainState = TrainState.StationEnter;
+                CheckInclineAndSetRotation(TrackType.StraightGround);
                 StartCoroutine(TrainStationEnter(other.gameObject));
                 _trainReplenishCoroutine = StartCoroutine(_trainMgr.ReplenishTrainFuelAndDurability());
                 break;
             case "Track_Curved_RU":
+                yield return CheckInclineAndSetRotation(TrackType.RightUp);
                 _trackType = TrackType.RightUp;
                 break;
             case "Track_Curved_RD":
+                yield return CheckInclineAndSetRotation(TrackType.RightDown);
                 _trackType = TrackType.RightDown;
                 break;
             case "Track_Curved_LU":
+                yield return CheckInclineAndSetRotation(TrackType.LeftUp);
                 _trackType = TrackType.LeftUp;
                 break;
             case "Track_Curved_LD":
+                yield return CheckInclineAndSetRotation(TrackType.LeftDown);
                 _trackType = TrackType.LeftDown;
                 break;
             case "Track_LR":
             case "Track_TD":
+                yield return CheckInclineAndSetRotation(TrackType.StraightGround);
                 _trackType = TrackType.StraightGround;
+                break;
+            case "BridgeLR":
+            case "BridgeTD":
+                yield return CheckInclineAndSetRotation(TrackType.StraightBridge);
+                _trackType = TrackType.StraightBridge;
+                break;
+            case "SlopeLR":
+            case "SlopeTD":
+                if (_prevTrackType == TrackType.StraightBridge)
+                {
+                    yield return CheckInclineAndSetRotation(TrackType.InclineDown);
+                    _trackType = TrackType.InclineDown;
+                }
+                else if (_prevTrackType == TrackType.InclineUp || _prevTrackType == TrackType.InclineDown)
+                {
+                    break;
+                }
+                else
+                {
+                    yield return CheckInclineAndSetRotation(TrackType.InclineUp);
+                    _trackType = TrackType.InclineUp;
+                }
                 break;
             default:
                 Debug.LogError($"[TrainMovement] {this.name}: Invalid Tag in the Train's Trigger Zone");
@@ -155,9 +190,32 @@ public class TrainMovement : MonoBehaviour
     {
         while (_trainState == TrainState.StationDeparted)
         {
+
+            if (_trackType == TrackType.InclineUp || _trackType == TrackType.InclineDown)
+            {
+                
+            }
+            else if (_trackType == TrackType.StraightBridge)
+            {
+                //CheckInclineAndSetRotation();
+                _trainRigidbody.position = new Vector3(_trainRigidbody.position.x,
+                                                        _trainRigidbody.position.y,
+                                                        -4
+                                                        );
+            }
+            else
+            {
+                //CheckInclineAndSetRotation();
+                _trainRigidbody.position = new Vector3(_trainRigidbody.position.x,
+                                                        _trainRigidbody.position.y,
+                                                        -1
+                                                        );
+            }
+
             switch (_trackType)
             {
                 case TrackType.StraightGround:
+                case TrackType.StraightBridge:
                     MoveTrainStraight(MovementDirn);
                     break;
                 case TrackType.RightUp:
@@ -171,6 +229,10 @@ public class TrainMovement : MonoBehaviour
                     break;
                 case TrackType.LeftDown:
                     yield return StartCoroutine(MoveTrainLeftDown(MovementDirn));
+                    break;
+                case TrackType.InclineUp:
+                case TrackType.InclineDown:
+                    MoveTrainIncline(MovementDirn, _trackType);
                     break;
                 default:
                     Debug.LogError("[TrainMovement] MoveTrain Switch Case Not Implemented Error");
@@ -235,9 +297,121 @@ public class TrainMovement : MonoBehaviour
         return false;
     }
 
+    // Checks if there is an incline change
+    // If there is, move abit more and then set the rotation of the train
+    // Else just return.
+    private IEnumerator CheckInclineAndSetRotation(TrackType incomingTrack)
+    {
+        if (!CheckInclineChange(_prevTrackType, incomingTrack)) yield break;
+        yield return MoveFixedDistance();
+        
+        
+        int tiltAngle = 45;
+        if (incomingTrack == TrackType.InclineDown)
+        {
+            tiltAngle *= -1;
+        }
+        else if (incomingTrack == TrackType.InclineUp)
+        {
+            tiltAngle *= 1;
+        } 
+        else
+        {
+            tiltAngle = 0;
+        }
+
+
+
+        if (MovementDirn == TrainDirection.EAST || MovementDirn == TrainDirection.SOUTH)
+        {
+            tiltAngle *= 1;
+        } 
+        else if (MovementDirn == TrainDirection.WEST || MovementDirn == TrainDirection.NORTH)
+        {
+            tiltAngle *= -1;
+        }
+
+
+
+
+        if (incomingTrack == TrackType.InclineDown || incomingTrack == TrackType.InclineUp)
+        {
+            if (_collidedObject.tag == "SlopeLR")
+            {
+
+                _trainRigidbody.transform.rotation = Quaternion.Euler(  _trainRigidbody.transform.eulerAngles.x,
+                                                                        tiltAngle,
+                                                                        _trainRigidbody.transform.eulerAngles.z
+                                                                        );
+            } 
+            else if (_collidedObject.tag == "SlopeTD")
+            {
+                _trainRigidbody.transform.rotation = Quaternion.Euler(  tiltAngle,
+                                                                        _trainRigidbody.transform.eulerAngles.y,
+                                                                        _trainRigidbody.transform.eulerAngles.z
+                                                                        );
+            } 
+            else
+            {
+                Debug.LogError("Not possible to be on inclide and yet not on a slope");
+            }
+        }
+        else
+        {
+            _trainRigidbody.transform.rotation = Quaternion.Euler(  0,
+                                                                    0,
+                                                                    _trainRigidbody.transform.eulerAngles.z
+                                                                    );
+        }
+    }
+
+    private bool CheckInclineChange(TrackType prevTrack, TrackType incomingTrack)
+    {
+        switch (prevTrack)
+        {
+            case TrackType.RightUp:
+            case TrackType.RightDown:
+            case TrackType.LeftUp:
+            case TrackType.LeftDown:
+            case TrackType.StraightGround:
+                if (incomingTrack == TrackType.InclineUp) return true;
+                break;
+            case TrackType.StraightBridge:
+                if (incomingTrack == TrackType.InclineDown) return true;
+                break;
+            case TrackType.InclineUp:
+                if (incomingTrack == TrackType.StraightBridge) return true;
+                break;
+            case TrackType.InclineDown:
+                if (incomingTrack != TrackType.InclineDown && incomingTrack != TrackType.InclineUp && incomingTrack != TrackType.StraightBridge) return true;
+                break;
+            case TrackType.Nil:
+                break;
+            default:
+                Debug.LogError("Unaccounted Track type for checking");
+                break;
+
+        }
+        return false;
+    }
+
     //////////////////////////////////////////////////////
     /// STRAIGHT MOVEMENT LOGIC
     //////////////////////////////////////////////////////
+    ///
+
+    // Called by the CheckInclineAndSetRotation function to make the train move abit more before performing the rotation
+    private IEnumerator MoveFixedDistance()
+    {
+        Vector3 initialPosition = _trainRigidbody.position;
+        float distanceTravelled = Vector3.Distance(initialPosition, _trainRigidbody.position);
+
+        while (distanceTravelled < 1.5f)
+        {
+            yield return null;
+            distanceTravelled = Vector3.Distance(initialPosition, _trainRigidbody.position);
+        }
+    }
 
     private void MoveTrainStraight(TrainDirection currentDirn)
     {
@@ -259,6 +433,49 @@ public class TrainMovement : MonoBehaviour
                 Debug.LogError($"[TrainMovement] {this.name}: Invalid Direction being used to move in a straight line");
                 break;
         }
+    }
+
+    private void MoveTrainIncline (TrainDirection currDirn, TrackType incline)
+    {
+        if (incline != TrackType.InclineUp && incline != TrackType.InclineDown)
+        {
+            Debug.LogError("Non-incline track should not call this MoveTrainIncline!");
+        }
+
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+        switch (currDirn){
+            case TrainDirection.NORTH:
+                y = CurrentSpeed;
+                break;
+            case TrainDirection.SOUTH:
+                y = -CurrentSpeed;
+                break;
+            case TrainDirection.EAST:
+                x = CurrentSpeed;
+                break;
+            case TrainDirection.WEST:
+                x = -CurrentSpeed;
+                break;
+            default:
+                break;
+        }
+
+        switch (incline)
+        {
+            case TrackType.InclineUp:
+                z = -CurrentSpeed;
+                break;
+            case TrackType.InclineDown:
+                z = CurrentSpeed;
+                break;
+            default:
+                break;
+
+        }
+        _trainRigidbody.velocity = new Vector3(x, y, z);
     }
 
 
