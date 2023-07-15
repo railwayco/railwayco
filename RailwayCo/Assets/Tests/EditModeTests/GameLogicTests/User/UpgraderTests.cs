64ac4a3535e53436860eb9e47f8495418c9cf55d
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-15 14:32:58 +0800

    chore: move and rename tests
    Upgrade to Upgrader

---
+++ RailwayCo/Assets/Tests/EditModeTests/GameLogicTests/User/UpgraderTests.cs
@@ -0,0 +1,45 @@
+using System;
+using NUnit.Framework;
+
+public class UpgraderTests
+{
+    [TestCase(0, 50)]
+    [TestCase(int.MaxValue, 1)]
+    public void Upgrader_AddSkillPoint_SkillPointIncreased(int basePoint, int skillPoint)
+    {
+        Upgrader upgrader = UpgraderInit(basePoint);
+        upgrader.AddSkillPoint(skillPoint);
+        Assert.AreEqual(Arithmetic.IntAddition(basePoint, skillPoint), upgrader.SkillPoint);
+    }
+
+    [TestCase(0, -50)]
+    [TestCase(int.MinValue, -1)]
+    public void Upgrader_AddSkillPoint_SkillPointInvalid(int basePoint, int skillPoint)
+    {
+        Upgrader upgrader = UpgraderInit(basePoint);
+        Assert.Catch<ArgumentException>(() => upgrader.AddSkillPoint(skillPoint));
+    }
+
+    [TestCase(0, 50)]
+    [TestCase(int.MaxValue, 1)]
+    public void Upgrader_RemoveSkillPoint_SkillPointDecreased(int basePoint, int skillPoint)
+    {
+        Upgrader upgrader = UpgraderInit(basePoint);
+        upgrader.RemoveSkillPoint(skillPoint);
+        Assert.AreEqual(Arithmetic.IntSubtraction(basePoint, skillPoint), upgrader.SkillPoint);
+    }
+
+    [TestCase(0, -50)]
+    [TestCase(int.MinValue, -1)]
+    public void Upgrader_RemoveSkillPoint_SkillPointInvalid(int basePoint, int skillPoint)
+    {
+        Upgrader upgrader = UpgraderInit(basePoint);
+        Assert.Catch<ArgumentException>(() => upgrader.RemoveSkillPoint(skillPoint));
+    }
+
+    private Upgrader UpgraderInit(int skillPoint)
+    {
+        Upgrader upgrader = new(skillPoint);
+        return upgrader;
+    }
+}
