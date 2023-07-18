fd38f5772c7a619bb56f807ba809167f25c2ec49
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-19 01:54:47 +0800

    chore: remove unused usings

--- RailwayCo/Assets/Scripts/Unity/Main/LogicManager.cs
+++ RailwayCo/Assets/Scripts/Unity/Main/LogicManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 // Intermediary between all the GameObjects and Backend GameManeger/GameLogic
 public class LogicManager : MonoBehaviour
