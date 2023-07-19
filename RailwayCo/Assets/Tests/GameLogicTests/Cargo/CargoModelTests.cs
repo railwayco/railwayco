bf763b7ef028349a9de0362da85239af77d04af0
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-19 22:42:53 +0800

    chore: remove unused usings

--- RailwayCo/Assets/Tests/GameLogicTests/Cargo/CargoModelTests.cs
+++ RailwayCo/Assets/Tests/GameLogicTests/Cargo/CargoModelTests.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 
 public class CargoModelTests
 {
--- RailwayCo/Assets/Tests/GameLogicTests/Station/StationMasterTests.cs
+++ RailwayCo/Assets/Tests/GameLogicTests/Station/StationMasterTests.cs
@@ -1,6 +1,5 @@
 using NUnit.Framework;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 
 public class StationMasterTests
