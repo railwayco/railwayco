d6fd76e97f8752011a12551bfe9f3cf78ff0e734
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-24 20:39:33 +0800

    fix: webgl aot deserialisation

---
+++ RailwayCo/Assets/Scripts/GameLogic/GameLogicAOT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Newtonsoft.Json.Utilities;
+
+internal class GameLogicAOT : MonoBehaviour
+{
+    private void Awake()
+    {
+        AotHelper.Ensure(() => new User("", 0, new(0), new()));
+        AotHelper.EnsureDictionary<Guid, Cargo>();
+        AotHelper.EnsureDictionary<Guid, Train>();
+        AotHelper.EnsureDictionary<Guid, Station>();
+        AotHelper.EnsureDictionary<Guid, Platform>();
+        AotHelper.EnsureList<Guid>();
+    }
+}
