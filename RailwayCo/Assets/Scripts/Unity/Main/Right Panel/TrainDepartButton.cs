166924db79d7d19713f2ea42e85480bd9bd91b59
wenenhoe <33289235+wenenhoe@users.noreply.github.com>
2023-07-29 23:50:39 +0800

    fix: format depart cost string

--- RailwayCo/Assets/Scripts/Unity/Main/Right Panel/TrainDepartButton.cs
+++ RailwayCo/Assets/Scripts/Unity/Main/Right Panel/TrainDepartButton.cs
@@ -108,7 +108,7 @@ public class TrainDepartButton : MonoBehaviour, IPointerExitHandler
         string destinationString = _destStationNum == 0 ? "No Station" : $"Station {_destStationNum}";
         string costString = _departCost.ToString();
         this.transform.Find("Depart text").GetComponent<Text>().text = destinationString;
-        this.transform.Find("Cost text").GetComponent<TMP_Text>().text += costString;
+        this.transform.Find("Cost text").GetComponent<TMP_Text>().text = $"Cost: {costString} Fuel";
     }
 
 
